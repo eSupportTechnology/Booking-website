2701d4fa4ad112f2d09310048f037bb5bba71139
IT22251428 <it22251428@my.sliit.lk>
2025-05-09 10:06:15 +0530

    Property Page

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'My Laravel App')</title>
+      <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
 
     {{-- Google Fonts: Lato --}}
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
@@ -12,7 +14,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
-<body class="font-sans text-gray-900">
+<body class=" text-gray-900" style="font-family: 'Lato', sans-serif;">
   <!-- Include Header -->
   @include('frontend.Header')
 
