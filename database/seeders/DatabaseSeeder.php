23c33a32a220df942ac5d9b3482d640e0aa9cd81
GTec <gayashancs7@gmail.com>
2025-07-16 11:49:06 +0530

    Fix seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class,
             PropertyCategoriesTableSeeder::class,
-            PropertySubcategoriesTableSeeder::class,
+            // PropertySubcategoriesTableSeeder::class,
             PropertyHotelSubcategorySeeder::class,
             PropertySubcategorySeeder::class,
             PropertySubtypeSeeder::class,
