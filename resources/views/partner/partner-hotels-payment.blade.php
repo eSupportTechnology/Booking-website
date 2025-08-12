8b4e0e14ff567a22c29a1e9c32e8b85417b7f8b5
Hasiru Herath <hasiruherath.13@gmail.com>
2025-08-12 17:32:50 +0530

    Updated  property editing in step 1 of partner flow

--- resources/views/partner/partner-hotels-payment.blade.php
+++ resources/views/partner/partner-hotels-payment.blade.php
@@ -196,7 +196,7 @@
                     showToast('Payment process completed successfully!', 'success');
                     // Redirect to a GET route after successful completion
                     setTimeout(() => {
-                        window.location.href =`/partner-homes-edit/${this.propertyId}?propertyType=single&paymentDetails=true$uploaded=true`;
+                        window.location.href =`/partner-homes-edit/${this.propertyId}?propertyType=single&paymentDetails=true&uploaded=true&rooms=true`;
                     }, 2000);
                 } else {
                     showToast('Error: ' + data.message, 'error');
