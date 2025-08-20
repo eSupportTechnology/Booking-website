f1e51cc1dcdfdccf378525da06f0954fbab182ea
Hasiru Herath <hasiruherath.13@gmail.com>
2025-08-20 12:32:00 +0530

    new migrations  for car rental

---
+++ app/Models/CarBrand.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CarBrand extends Model
+{
+    protected $fillable = ['brand_name'];
+
+    public function models()
+    {
+        return $this->hasMany(CarModel::class, 'brand_id');
+    }
+}
