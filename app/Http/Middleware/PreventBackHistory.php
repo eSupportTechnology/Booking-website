374e82232e90842b5fb47b6fe5dbf14669f23316
Madusanka-Max <isuru.rodrigo1243@gmail.com>
2025-08-25 10:25:52 +0530

    change

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -10,9 +10,10 @@ class PreventBackHistory
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-        
+
         return $response->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
                        ->header('Pragma', 'no-cache')
                        ->header('Expires', 'Sat, 01 Jan 1990 00:00:00 GMT');
+                       
     }
 }
