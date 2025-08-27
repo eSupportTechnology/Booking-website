26f3c26aac0dc640bed7bf2b13532584a64c4d1a
Madusanka-Max <isuru.rodrigo1243@gmail.com>
2025-08-27 12:04:18 +0530

    add rental part to admin from frontend

--- routes/web.php
+++ routes/web.php
@@ -808,19 +808,11 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('/customer/{customer_id}', [\App\Http\Controllers\Admin\CustomerViewController::class, 'show'])->name('customer.view');
         Route::get('/partner/{partner_id}', [\App\Http\Controllers\Admin\PartnerViewController::class, 'show'])->name('partner.view');
 
-        // Property managementt
-        Route::get('/units', function () {
-            return view('admin.admin-apartments');
-        })->name('apartments');
-        Route::get('/residences', function () {
-            return view('admin.admin-homes');
-        })->name('homes');
-        Route::get('/venues', function () {
-            return view('admin.admin-hotels');
-        })->name('hotels');
-        Route::get('/unique-stays', function () {
-            return view('admin.admin-alternative-places');
-        })->name('alternative.places');
+        // Property management
+        Route::get('/units', \App\Http\Controllers\Admin\ApartmentsController::class)->name('apartments');
+        Route::get('/residences', \App\Http\Controllers\Admin\HomesController::class)->name('homes');
+        Route::get('/venues', \App\Http\Controllers\Admin\HotelsController::class)->name('hotels');
+        Route::get('/unique-stays', \App\Http\Controllers\Admin\AlternativePlacesController::class)->name('alternative.places');
 
         // Customer management
         Route::get('/accounts', [\App\Http\Controllers\Admin\CustomersController::class, '__invoke'])->name('customers');
@@ -834,6 +826,10 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('/customer/{customer_id}', [\App\Http\Controllers\Admin\CustomerViewController::class, 'show'])->name('customer.view');
         Route::get('/partner/{partner_id}', [\App\Http\Controllers\Admin\PartnerViewController::class, 'show'])->name('partner.view');
 
+        // Admin Dashboard rental Route
+        Route::view('/admin/rental/taxi', 'admin-admin.taxi')->name('admin.taxi');
+        Route::view('/admin/rental/taxi/taxi-details', 'admin-admin.taxi-details')->name('admin.taxi.details');
+        Route::view('/admin/rental/airport', 'admin-admin.airport')->name('admin.airport');
 
 
         // Settings
