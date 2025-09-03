63d6cebb7ffea993106e3b364b8e79ccfe908c8f
Madusanka-Max <isuru.rodrigo1243@gmail.com>
2025-09-03 07:34:31 +0530

    partner view

--- app/Models/BusinessEntity.php
+++ app/Models/BusinessEntity.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BusinessEntity extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'accommodation_id',
@@ -20,10 +19,13 @@ class BusinessEntity extends Model
         'country',
     ];
 
-    protected $dates = ['deleted_at'];
-
     public function accommodation()
     {
         return $this->belongsTo(Accommodation::class);
+    }
+
+    public function property()
+    {
+        return $this->hasOneThrough(Property::class, Accommodation::class);
     }
 }
