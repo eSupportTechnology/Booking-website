7267480107284f0365b07973a05bfc598d021a6f
Madusanka-Max <isuru.rodrigo1243@gmail.com>
2025-09-03 07:34:31 +0530

    partner view

--- app/Http/Controllers/Admin/PartnerViewController.php
+++ app/Http/Controllers/Admin/PartnerViewController.php
@@ -32,5 +32,7 @@ class PartnerViewController extends Controller
         ->findOrFail($partner_id);
 
         return view('admin.admin-partner-view', compact('partner'));
+
+        return view('admin.admin-partner-view', compact('partner'));
     }
 }
