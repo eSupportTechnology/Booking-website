e2bece574010a8fc812b9a794a19396c6661a928
SandulRusara <sandulrusara8@gmail.com>
2025-09-26 12:50:46 +0530

    button update

--- resources/views/frontend/car-rentals.blade.php
+++ resources/views/frontend/car-rentals.blade.php
@@ -125,14 +125,16 @@
         <div class="border-r border-black h-8"></div>
       </div>
 
+      
       <!-- Submit Button -->
-      <div class="flex-shrink-0 w-full md:w-auto">
+        <div class="flex-shrink-0 w-full md:w-auto md:self-center">
         <button type="submit"
-          class="w-full md:w-auto bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-2 rounded-lg text-sm"
-          style="background-color:#3CC0E9;">
-          Search
+            class="w-full md:w-auto bg-blue-600 hover:bg-blue-700 text-white font-semibold px-4 py-[10px] rounded-lg text-sm"
+            style="background-color:#3CC0E9;">
+            Search
         </button>
-      </div>
+        </div>
+
     </div>
   </form>
 
