71910023b67bba976e35ad45c204163dbf35aa7c
SandulRusara <sandulrusara8@gmail.com>
2025-10-02 10:21:26 +0530

    testing

--- resources/views/frontend/car-rentals.blade.php
+++ resources/views/frontend/car-rentals.blade.php
@@ -178,8 +178,6 @@
 
 
 
-
-
 <section class="py-12 bg-white">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 class="text-2xl font-semibold text-gray-800 mb-6">Popular car hire brands</h2>
