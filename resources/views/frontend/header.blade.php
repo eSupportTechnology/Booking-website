c4729bc7db62d52fd2ae3497011dd680c675d28d
SandulRusara <sandulrusara8@gmail.com>
2025-10-07 13:52:40 +0530

    change icons

--- resources/views/frontend/header.blade.php
+++ resources/views/frontend/header.blade.php
@@ -82,7 +82,7 @@
                 </a>
 
                 <a href="{{ route('customer.bookings.index') }}" class="block px-4 py-2 text-gray-700 text-base hover:bg-gray-100 flex items-center gap-2">
-                     <img src="{{ asset('assets/mynaui_letter-g-circle.svg') }}" class="w-5 h-5" /> My Bookings
+                     <img src="{{ asset('assets/balcony.svg') }}" class="w-5 h-5" /> My Bookings
                 </a>
                              
                 {{-- 
