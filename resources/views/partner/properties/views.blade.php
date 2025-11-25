86e11727bcc12a30f705e1d81770a38e685a12bc
SandulRusara <sandulrusara8@gmail.com>
2025-11-25 13:58:44 +0530

    partner title update

--- resources/views/partner/properties/views.blade.php
+++ resources/views/partner/properties/views.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8" />
-    <title>Partner Dashboard</title>
+    <title>View</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdn.tailwindcss.com"></script>
